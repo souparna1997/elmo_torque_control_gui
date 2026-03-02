c0b33793691c273ba4882ab23952f012a5f20c84
souparna1997 <souparna@stanford.edu>
2026-03-01 17:45:03 -0800

    Added start/stop buttons to start/stop plotting. Removed last 5 sec plotting method, instead now it shows a time incremental plot; Also made the gui only read from the shared memory

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,17 +9,30 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "shared_data.h"
-
+#include <QVBoxLayout>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow),
+    : QMainWindow(parent) //Initialize base class
+    , ui(new Ui::MainWindow), //Initialize UI Pointer
     shm_ptr(nullptr),
-    last_index(0)
+    last_index(0) //Initialize last index value to zero
+    , first_timestamp(0)
 {
-    ui->setupUi(this);
+    ui->setupUi(this); //Populate window with widgets from .ui
 
-    // Create plot series
+    QWidget *central = new QWidget(this);
+    QVBoxLayout *layout = new QVBoxLayout(central);
+
+    // Create Start and Stop Button
+    QHBoxLayout *buttonLayout  = new QHBoxLayout;
+
+    startButton = new QPushButton("Start Torque Plot");
+    startButton->setCheckable(true);
+
+    layout->addWidget(startButton);
+
+    // Create line series for plotting
     torqueSeries = new QLineSeries();
 
     // Create chart and attach series
@@ -27,14 +40,11 @@ MainWindow::MainWindow(QWidget *parent)
     chart->addSeries(torqueSeries);
     chart->setTitle("Motor Torque (Actual)");
 
-    // Connect shared memory
-    connectSharedMemory();
-    if (!shm_ptr) qDebug() << "Shared memory not connected!";
-
     // Create axes
     axisX = new QValueAxis;
     axisX->setTitleText("Time (s)");
     axisX->setLabelFormat("%.2f");
+    //axisX->setRange(0,10); //Initial Range
 
     axisY = new QValueAxis;
     axisY->setTitleText("Torque (Actual)");
@@ -53,14 +63,19 @@ MainWindow::MainWindow(QWidget *parent)
     chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
 
-    //Set chartView as the central widget of main window
-    setCentralWidget(chartView);
+    layout->addWidget(chartView);
 
-    // Timer at 50 Hz
+    //Set chartView as the central widget of main window
+    setCentralWidget(central);
+
+    // Timer at 1 Hz (do not start yet)
     timer = new QTimer(this);
     timer->setTimerType(Qt::PreciseTimer); // This is the secret sauce
     connect(timer, &QTimer::timeout, this, &MainWindow::updatePlot);
-    timer->start(1000);
+    //timer->start(1000);
+
+    // Connect start Button
+    connect(startButton, &QPushButton::clicked, this, &MainWindow::togglePlot);
 }
 
 void MainWindow::connectSharedMemory()
@@ -79,45 +94,89 @@ void MainWindow::connectSharedMemory()
         MAP_SHARED,
         fd,
         0);
+
+    ::close(fd);
+
+    if (shm_ptr == MAP_FAILED)
+    {
+        qDebug("mmap failed!");
+        shm_ptr = nullptr;
+        return;
+    }
+    
+
 }
+
+void MainWindow::togglePlot()
+{
+    if (startButton->isChecked()) {
+        qDebug() << "Plotting started";
+        connectSharedMemory();
+
+        if (!shm_ptr) {
+            qDebug() << "Shared memory not available!";
+            startButton->setChecked(false);
+            return;
+        }
+
+        last_index = shm_ptr->write_index;
+        first_timestamp = 0;
+
+        timer->start(10);  // adjust frequency
+        startButton->setText("Stop Torque Plot");
+
+    } else {
+        qDebug() << "Plotting stopped";
+        timer->stop();
+        startButton->setText("Start Torque Plot");
+
+        //Clear PLot on Stop
+        torqueSeries->clear();
+        last_index = shm_ptr->write_index;
+        first_timestamp = 0;
+    }
+}
+
 
 void MainWindow::updatePlot()
 {
     if (!shm_ptr) return;
 
-    uint32_t current_index = shm_ptr->write_index;
+    // Read the data from the buffer
+    uint32_t current_index = shm_ptr->write_index; //current_index is total samples written by the controller so far
     if (current_index == 0) return;  // no data yet
 
     double last_time_sec = 0.0;
 
-    // Append all new samples
+    // Append all new samples that haven't been plotted yet
     while (last_index < current_index)
     {
+        // Fetch the sample from the circular buffer (%BUFFER_SIZE wraps around if the controller has overwritten old samples)
         MotorSample &s = shm_ptr->buffer[last_index % BUFFER_SIZE];
 
+        //Record the timestamp for the VERY FIRST sample plotted (This becomes the time-origin x = 0 on the plot)
         if (first_timestamp == 0)
             first_timestamp = s.timestamp;
 
-        last_time_sec = (s.timestamp - first_timestamp) * 1e-9;
+        last_time_sec = (s.timestamp - first_timestamp) * 1e-9; //Convert ns to s for plotting
+        axisX->setRange(0, last_time_sec); // expand to include new points
 
-        torqueSeries->append(last_time_sec, s.torque_actual);
+        torqueSeries->append(last_time_sec, s.torque_actual); //Append the new data to the chart
 
         // qDebug() << "Timestamp:" << s.timestamp
         //          << "Torque:" << s.torque_actual;
 
-        last_index++;
+        last_index++; //last index moves forward as we append samples
     }
 
-    // Keep last 5 seconds visible
-    axisX->setRange(last_time_sec - 5, last_time_sec);
-
     // Limit number of points to prevent slowdown
-    if (torqueSeries->count() > 1000)
-        torqueSeries->removePoints(0, torqueSeries->count() - 1000);
+    if (torqueSeries->count() > 20000)
+        torqueSeries->removePoints(0, torqueSeries->count() - 20000);
 
     // Force chart redraw
     //chart->update();
 }
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,8 @@
 #include <QtCharts/QValueAxis> //Numeric axis(x/y)
 #include <QTimer> //Timer for periodic updates
 #include "shared_data.h" // Single Producer Single Consumer Circular Buffer
+#include <QVBoxLayout>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -24,6 +26,7 @@ public:
 
 private slots:
     void updatePlot();
+    void togglePlot();
 
 private:
     Ui::MainWindow *ui;
@@ -33,14 +36,17 @@ private:
     QChartView *chartView;
     QTimer *timer;
 
-    SharedMemory *shm_ptr;
-    uint32_t last_index;
+    SharedMemory *shm_ptr; // pointer to the shared memory (read-only on GUI side)
+    uint32_t last_index; // Tracks the last sample that has been plotted
 
-    uint64_t first_timestamp = 0; // member variable
+    uint64_t first_timestamp = 0; // member variable (Used to normalize the first sample so that the X-axis starts at 0)
     double time_sec = 0.0;   // member variable
 
     QValueAxis *axisX;
     QValueAxis *axisY;
+
+    QPushButton *startButton;
+    QPushButton *stopButton;
 
     void connectSharedMemory();
 };
